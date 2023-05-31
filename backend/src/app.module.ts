6ecf335a7426c366a6c5d8ed5719fcf4beef9b5f
rafael556 <rafael7jf@hotmail.com>
2023-05-31 01:03:31 -0300

    feat: connect auth database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,23 @@ import { ReportModule } from './report/report.module';
 import { LoginModule } from './login/login.module';
 import { Module } from '@nestjs/common';
 import { VehicleModule } from './vehicle/vehicle.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ReportModule, VehicleModule, LoginModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [__dirname + `/**/*.entity{.js, .ts}`],
+      synchronize: true,
+    }),
+    ReportModule,
+    VehicleModule,
+    LoginModule,
+  ],
 })
 export class AppModule {}
