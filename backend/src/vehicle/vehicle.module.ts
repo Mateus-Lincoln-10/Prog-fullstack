e9fc7158918e9ad81d268cafa90be808162eb64b
rafael556 <rafael7jf@hotmail.com>
2023-05-31 01:05:20 -0300

    fix: remove imports of nonexistent files

--- backend/src/vehicle/vehicle.module.ts
+++ backend/src/vehicle/vehicle.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VehicleService } from './vehicle.service';
 import { VehicleController } from './vehicle.controller';
-import { VehicleAssembler } from './vehicleAssembler.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { join } from 'path';
-
 @Module({
   imports: [],
-  providers: [VehicleService, VehicleAssembler],
+  providers: [VehicleService],
   controllers: [VehicleController],
 })
 export class VehicleModule {}
