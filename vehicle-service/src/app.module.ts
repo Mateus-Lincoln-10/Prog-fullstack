0c6834740529dafe62c4187800502ad9abd806d6
rafael556 <rafael7jf@hotmail.com>
2023-06-01 00:49:43 -0300

    feat: connect vehicle database credentials

--- vehicle-service/src/app.module.ts
+++ vehicle-service/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VehicleModule } from './vehicle/vehicle.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    VehicleModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'vehicle',
+      password: 'root',
+      database: 'vehicle',
+      entities: [__dirname + `/**/*.entity{.js, .ts}`],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
