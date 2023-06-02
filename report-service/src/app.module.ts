c17a66c5d62ad97b1a438ed8396bbd41034c5a8a
rafael556 <rafael7jf@hotmail.com>
2023-06-02 01:59:52 -0300

    feature: setup report database

--- report-service/src/app.module.ts
+++ report-service/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ReportModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5434,
+      username: 'report',
+      password: 'root',
+      database: 'report',
+      entities: [__dirname + `/**/*.entity{.js, .ts}`],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
