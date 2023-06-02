35ca8c6dfd7fc5074249234d9ef6bf8a8fdb1b14
Mateus lincoln <mateus@g4tech.com.br>
2023-06-02 20:24:50 -0300

    feat: configurar-cache

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { LoginModule } from './login/login.module';
 import { Module } from '@nestjs/common';
 import { VehicleModule } from './vehicle/vehicle.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
+    CacheModule.register({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
