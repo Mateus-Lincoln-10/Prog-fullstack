19b25da3cad7d0f2aeae2e9936a3989ee74751f0
rafael556 <rafael7jf@hotmail.com>
2023-06-02 02:05:45 -0300

    feat: create gateway report controller

--- backend/src/report/report.controller.ts
+++ backend/src/report/report.controller.ts
@@ -1,8 +1,33 @@
-/*
-https://docs.nestjs.com/controllers#controllers
-*/
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ReportService } from './report.service';
+import { ApiBearerAuth, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/login/guards/auth.guard';
 
-import { Controller } from '@nestjs/common';
+@ApiBearerAuth('access-token')
+@UseGuards(AuthGuard)
+@ApiTags('Report Controller')
+@Controller('/report')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
 
-@Controller()
-export class ReportController {}
+  @ApiCreatedResponse({
+    description: 'The report request has been successfully sent.',
+  })
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  createReports() {
+    return { message: this.reportService.createReport() };
+  }
+
+  @Get()
+  async listReports() {
+    return await this.reportService.listReports();
+  }
+}
--- backend/src/report/report.module.ts
+++ backend/src/report/report.module.ts
@@ -1,11 +1,10 @@
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
-
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [],
-  controllers: [ReportController],
-  providers: [ReportService],
+  controllers: [ReportController, ReportController],
+  providers: [ReportService, ReportService],
 })
 export class ReportModule {}
