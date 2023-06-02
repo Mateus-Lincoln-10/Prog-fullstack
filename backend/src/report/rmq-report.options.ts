1aaea8d5df650abc66299f706d6733345b276d07
rafael556 <rafael7jf@hotmail.com>
2023-06-02 02:04:19 -0300

    fix: adjust rmq handhsake

--- backend/src/report/rmq-report.options.ts
+++ backend/src/report/rmq-report.options.ts
@@ -3,7 +3,7 @@ import { ClientOptions, Transport } from '@nestjs/microservices';
 export const rmqReportOptions: ClientOptions = {
   transport: Transport.RMQ,
   options: {
-    urls: ['amqp://localhost:5672'],
+    urls: ['amqp://admin:123456@localhost:5672'],
     queue: 'report_queue',
     queueOptions: {
       durable: false,
