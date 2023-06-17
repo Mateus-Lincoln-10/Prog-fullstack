c916532b14d69d157adceb8679c3ecbe9a81ef4d
rafael556 <rafael7jf@hotmail.com>
2023-06-17 19:34:00 -0300

    feat: add https in vehicle service

--- vehicle-service/src/main.ts
+++ vehicle-service/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('../cert/private.key'),
+    cert: fs.readFileSync('../cert/certificate.crt'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   await app.listen(9002);
 }
