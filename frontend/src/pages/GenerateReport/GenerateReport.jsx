7ab3c47f76230991e49d3335f5cac3147bf5d61f
rafael556 <rafael7jf@hotmail.com>
2023-06-19 23:09:08 -0300

    feat: create report list

--- frontend/src/pages/GenerateReport/GenerateReport.jsx
+++ frontend/src/pages/GenerateReport/GenerateReport.jsx
@@ -1,59 +1,61 @@
-import {React, useState} from 'react';
+import {React, useEffect, useState} from 'react';
 import * as C from "./styles";
+import { useAuth } from './../../hooks/useAuth';
+import api from './../../services/api';
+import { useMemo } from 'react';
+import { useCallback } from 'react';
 
 const ReportList = () => {
-      const [reportId, setReportId] = useState("");
+      const [reportList, setReportList] = useState([]);
       const { isLogged } = useAuth();
 
-      const handleReport = async () => {
+      useEffect(() => {
+        handleReport()
+      },[])
+
+      const handleReport = useCallback(async () => {
         try {
           const response = await api.get('/report', {
             headers: {
               Authorization: `Bearer ${isLogged()}`
-            },
+            }
           });
-
-          if(response.data.length === 0) {
-            toast.warning(response.data.message,  {
-              position: "top-right",
-              autoClose: 5000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: false,
-              progress: undefined,
-              theme: "light"
-              })
-          }
-          setReportList(response.data)
-        } catch(e) {
+          console.log(response);
+          setReportList(response.data);
+        } catch (e) {
           console.error(e);
         }
-      }
+      }, [isLogged]);
+
+      const handleCards = useCallback((report) => {
+        return (
+          <C.Card key={report.reportId}>
+            <span><C.Strong>Data de criação: </C.Strong> <C.Label>{report.reportCreatedAt}</C.Label></span>
+            <span><C.Strong>URL: </C.Strong> <C.Label>{report.reportUrl}</C.Label></span>
+            <span><C.Strong>ID: </C.Strong> <C.Label>{report.reportId}</C.Label></span>
+          </C.Card>
+        );
+      }, []);
+
+      const cardList = useMemo(() => {
+        if(reportList.length === 0) {
+          return;
+        }
+        return reportList.map(handleCards);
+      }, [handleCards, reportList]);
 
   return (
     <C.Container>
 
-    <table className="report-table">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Url</th>
-          <th>Criado em</th>
-        </tr>
-      </thead>
-      <tbody>
-          <tr key={reportId}>
-            <td>{report.reportId}</td>
-            <td>{report.reportUrl}</td>
-            <td>{report.reportCreateAt}</td>
-          </tr>
-      </tbody>
-    </table>
+      <C.Title>Resultado da Pesquisa</C.Title>
+        {reportList.length > 0 && (
+          <C.CardContainer>
+            {cardList}
+          </C.CardContainer>
+        )}
     </C.Container>
   );
 };
-
 export default ReportList;
 
 
